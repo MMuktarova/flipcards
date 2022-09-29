0689af6d9810d15fdb84998c5964311fddb18af6
Meerim Muktarova <muktarovameerim@gmail.com>
2022-09-29 23:40:19 +0200

    1 part of code with some error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const mainUrl = 'https://api.unsplash.com/photos'
+const key = '67dzJ6HsOJ0oE9moFGN9_AGJ-de6jwkL-CAULtu3QWg'
+
+//https://api.unsplash.com/photos//?client_id=67dzJ6HsOJ0oE9moFGN9_AGJ-de6jwkL-CAULtu3QWg&page=3
+
+const App = () => {
+
+  const [listOfImages, setListOfImages] = ([]);
+
+  
+  
+  const handleFetching = () => {
+    const url1 = `${mainUrl}/?client_id=${key}&page=${3}`;
+    const url2 = `${mainUrl}/?client_id=${key}&page=${4}`;
+
+    axios.all([axios.get(url1), axios.get(url2)]).then(([page1, page2]) => {
+      console.log(page1.data, page2.data);
+      
+      let data = [
+        ...page1.data,
+        ...page1.data,
+        ...page2.data.slice(0, 2),
+        ...page2.data.slice(0, 2),
+      ];
+      
+      console.log(data)
+      setListOfImages(data);
+    });
+  };
+  
+  useEffect(() => {
+    handleFetching();
+  },[])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="App">
+        {listOfImages.map((item) => {
+          return (
+            <div className='single-card'>
+              <img src="" alt="" />
+            </div>
+          );
+        })}
+     
     </div>
+      <div id='click'>
+        <span>Clicks</span>
+        <span className='counter'>0</span>
+        <button>Reset</button>
+      </div>
+      </>
   );
 }
 
